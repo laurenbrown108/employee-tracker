abe5bd14332a61aeff3a9a8d27fb8ed7af83d300
Lauren Brown <laurenbrown108@gmail.com>
2020-10-05 21:35:30 -0400

    Add functions done.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 const consoleTable = require("console.table");
 const { NONAME } = require("dns");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -35,9 +35,9 @@ function askQuestions() {
             "None"
         ]),
         name: "choice"
-    }).then(response => {
-        console.log(response.choice)
-        switch(response.choice) {
+    }).then(answer => {
+        console.log(answer.choice)
+        switch(answer.choice) {
             case "Add department":
                 addDept();
                 break;
@@ -67,31 +67,94 @@ function askQuestions() {
 }
 
 function addDept() {
-    console.log("dept added")
+    inquirer.prompt({
+        type: "input",
+        message: "Please enter the department name",
+        name: "deptName"
+    }).then((answer) => {
+        connection.query(`INSERT INTO department (name) VALUES ("${answer.deptName}");`, (err, res) => {
+            if(err) 
+                throw err;
+            console.log((`"${answer.deptName}" department added!`));
+            askQuestions();
+        });
+    })
+    
 }
 function viewDept() {
     console.log("viewing dept")
 }
 function addEmployee() {
-    console.log("employee added")
-}
-function viewEmployees() {
-    console.log("viewing employee")
-}
-function addRole() {
-    console.log("role added")
-}
-function viewRoles() {
-    console.log("viewing role")
-}
-function updateRole() {
-    console.log("role updated")
-}
-function none() {
-    console.log("quitting")
-    connection.end();
-}
+    //console.log("employee added")
+    inquirer.prompt([
+    {
+        type: "input",
+        message: "Enter employee's first name",
+        name: "firstName"
+    },
+    {
+        type: "input",
+        message: "Enter employee's last name",
+        name: "lastName"
+    },
+    {
+        type: "input",
+        message: "Enter employee's ID",
+        name: "roleID"
+    },
+    {
+        type: "input",
+        message: "Enter the ID of the employee's manager if applicable",
+        name: "managerID"
+    }
+    ]).then((answer) => {
+        connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answer.firstName}", "${answer.lastName}", ${answer.roleID}, ${answer.managerID});`, (err, res) => {
+            if(err) 
+                throw err;
+            console.log((`Employee ${answer.firstName} ${answer.lastName} added!`));
+            askQuestions();
+        });
 
-
-  //include in last function
-  //connection.end();
+    })
+}
+// function viewEmployees() {
+//     console.log("viewing employee")
+// }
+ function addRole() {
+     console.log("role added")
+     inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter role title",
+            name: "roleTitle"
+        },
+        {
+            type: "input",
+            message: "Enter this role's salary",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "Enter the department ID for this role",
+            name: "departmentID"
+        }
+        ]).then((answer) => {
+            connection.query(`INSERT INTO role (title, salary, department_id) VALUES ("${answer.roleTitle}", ${answer.salary}, ${answer.departmentID});`, (err, res) => {
+                if(err) 
+                    throw err;
+                console.log((`Role ${answer.roleTitle} added!`));
+                askQuestions();
+            });
+    
+        })
+}
+// function viewRoles() {
+//     console.log("viewing role")
+// }
+// function updateRole() {
+//     console.log("role updated")
+// }
+ function none() {
+     console.log("quitting")
+     connection.end();
+}
